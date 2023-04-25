fad9f5d96d177aba7d0a1e6b6eeaac09f088f458
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-04-25 12:58:39 -0300

    feat: Add `FuncionarioService` class

---
+++ src/service/FuncionarioService.java
@@ -0,0 +1,17 @@
+package service;
+
+import funcionario.Funcionario;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FuncionarioService {
+    public List<Funcionario> funcionarios = new ArrayList<>();
+
+    public void adicionarFuncionario(String nome, LocalDate dataNascimento, BigDecimal salario, String funcao) {
+        Funcionario funcionario = new Funcionario(nome, dataNascimento, salario, funcao);
+        funcionarios.add(funcionario);
+    }
+}
