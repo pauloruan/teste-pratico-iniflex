e96f73bd608404365e6275c0642915623fe293e5
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-04-28 12:02:16 -0300

    perf: Performance changes to the `EmployeeService` service.

--- src/service/EmployeeService.java
+++ src/service/EmployeeService.java
@@ -19,6 +19,15 @@ public class EmployeeService {
     public List<Employee> employees = new ArrayList<>();
     public Map<String, List<Employee>> employeesByPosition = new HashMap<>();
 
+    public static void printEmployeeDetails(Employee employee) {
+        System.out.println("Nome: " + employee.getName()
+                + " - Data de Nascimento: " + employee.getFormattedBirthDate()
+                + " - Salário: " + employee.getFormattedSalary()
+                + " - Função: " + employee.getPosition()
+                + "."
+        );
+    }
+
     public void addEmployee(String name, LocalDate birthDate, BigDecimal salary, String position) {
         Employee employee = new Employee(name, birthDate, salary, position);
         employees.add(employee);
@@ -30,14 +39,7 @@ public class EmployeeService {
 
     public void printEmployees() {
         System.out.println("Funcionários:");
-        for (Employee employee : employees) {
-            System.out.println("Nome: " + employee.getName()
-                    + " - Data de Nascimento: " + employee.getFormattedBirthDate()
-                    + " - Salário: " + employee.getFormattedSalary()
-                    + " - Função: " + employee.getPosition()
-                    + "."
-            );
-        }
+        employees.forEach(EmployeeService::printEmployeeDetails);
         System.out.println("\n");
     }
 
@@ -45,42 +47,32 @@ public class EmployeeService {
         employee.setSalary(employee.getSalary().multiply(BigDecimal.valueOf(1 + percentage)));
     }
 
+    public void updateEmployeesSalaries(double percentage) {
+        employees.forEach(employee -> updateEmployeeSalary(employee, percentage));
+    }
+
     public void groupEmployeesByPosition() {
-        for (Employee employee : employees) {
+        employees.forEach(employee -> {
             if (!employeesByPosition.containsKey(employee.getPosition())) {
                 employeesByPosition.put(employee.getPosition(), new ArrayList<>());
             }
             employeesByPosition.get(employee.getPosition()).add(employee);
-        }
+        });
     }
 
     public void printEmployeesByPosition() {
         this.groupEmployeesByPosition();
-        for (String position : employeesByPosition.keySet()) {
+        employeesByPosition.forEach((position, employees) -> {
             System.out.println("Funcionários com função " + position + ":");
-            for (Employee employee : employeesByPosition.get(position)) {
-                System.out.println("Nome: " + employee.getName()
-                        + " - Data de Nascimento: " + employee.getFormattedBirthDate()
-                        + " - Salário: " + employee.getFormattedSalary()
-                        + " - Função: " + employee.getPosition()
-                        + "."
-                );
-            }
+            employees.forEach(EmployeeService::printEmployeeDetails);
             System.out.println("\n");
-        }
+        });
     }
 
-    public void filterEmployeesByBirthdayMonth(int mes) {
-        for (Employee employee : employees) {
-            if (employee.getBirthDate().getMonthValue() == mes) {
-                System.out.println("Nome: " + employee.getName()
-                        + " - Data de Nascimento: " + employee.getFormattedBirthDate()
-                        + " - Salário: " + employee.getFormattedSalary()
-                        + " - Função: " + employee.getPosition()
-                        + "."
-                );
-            }
-        }
+    public void filterEmployeesByBirthdayMonth(int month) {
+        employees.stream()
+                .filter(employee -> employee.getBirthDate().getMonthValue() == month)
+                .forEach(EmployeeService::printEmployeeDetails);
         System.out.println("\n");
     }
 
@@ -91,52 +83,40 @@ public class EmployeeService {
     }
 
     public void printOldestEmployee() {
-        Employee oldestEmployee = employees.get(0);
-        for (Employee employee : employees) {
-            if (employee.getBirthDate().getYear() < oldestEmployee.getBirthDate().getYear()) {
-                oldestEmployee = employee;
-            }
-        }
-        System.out.println("Funcionário com maior idade:");
-        System.out.println("Nome: " + oldestEmployee.getName()
-                + " - Idade: " + Period.between(oldestEmployee.getBirthDate(), LocalDate.now()).getYears()
-                + "."
-        );
-        System.out.println("\n");
+        employees.stream()
+                .min(Comparator.comparing(employee -> employee.getBirthDate().getYear()))
+                .ifPresent(employee -> {
+                    System.out.println("Funcionário com maior idade:");
+                    System.out.println("Nome: " + employee.getName()
+                            + " - Idade: " + Period.between(employee.getBirthDate(), LocalDate.now()).getYears()
+                            + ".\n"
+                    );
+                });
     }
 
     public void printEmployeesInAlphabeticalOrder() {
-        List<Employee> employeesSorted = new ArrayList<>(employees);
-        employeesSorted.sort(Comparator.comparing(Person::getName));
         System.out.println("Funcionários em ordem alfabética:");
-        for (Employee employee : employeesSorted) {
-            System.out.println("Nome: " + employee.getName()
-                    + " - Data de Nascimento: " + employee.getFormattedBirthDate()
-                    + " - Salário: " + employee.getFormattedSalary()
-                    + " - Função: " + employee.getPosition()
-                    + "."
-            );
-        }
+        employees.stream()
+                .sorted(Comparator.comparing(Person::getName))
+                .forEach(EmployeeService::printEmployeeDetails);
         System.out.println("\n");
     }
 
     public void printTotalSalaries() {
-        BigDecimal totalSalaries = BigDecimal.ZERO;
-        NumberFormat formattedTotalSalaries = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
-        for (Employee employee : employees) {
-            totalSalaries = totalSalaries.add(employee.getSalary());
-        }
-        System.out.println("Total dos salários dos funcionários: " + formattedTotalSalaries.format(totalSalaries) + "\n");
+        BigDecimal totalSalaries = employees.stream()
+                .map(Employee::getSalary)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        System.out.println("Soma total dos salários dos funcionários: " + NumberFormat.getCurrencyInstance(new Locale("pt", "BR")).format(totalSalaries) + "\n");
     }
 
     public void printMinimumWages() {
         BigDecimal minimumWage = BigDecimal.valueOf(1212.00);
-        for (Employee employee : employees) {
+        employees.forEach(employee -> {
             System.out.println("Nome: " + employee.getName()
                     + " - Salário: " + employee.getFormattedSalary()
-                    + " - Salários Mínimos: " + employee.getSalary().divide(minimumWage,2, RoundingMode.FLOOR)
+                    + " - Salários Mínimos: " + employee.getSalary().divide(minimumWage, 2, RoundingMode.FLOOR)
             );
-        }
+        });
         System.out.println("\n");
     }
 }
